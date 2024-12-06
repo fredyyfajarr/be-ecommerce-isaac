848cbd7189d8e162675eb0ab2703ff6ddc4472c9
fredyyfajarr <fredyfajar@gmail.com>
2024-12-06 21:56:57 +0700

    update 1

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import { notFound, errorHandler } from './middlewares/errorMiddleware.js';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
 import ExpressMongoSanitize from 'express-mongo-sanitize';
 
+dotenv.config();
+
 const app = express();
 const port = 3000;
 
-import db from './utils/db.js';
 import authRouter from './routes/authRouter.js';
 import productRouter from './routes/productRouter.js';
 import orderRouter from './routes/orderRouter.js';
@@ -27,6 +30,16 @@ app.use('/api/v1/order', orderRouter);
 
 app.use(notFound);
 app.use(errorHandler);
+
+// Database
+mongoose
+  .connect(process.env.DATABASE, {})
+  .then(() => {
+    console.log('Database Connected');
+  })
+  .catch((error) => {
+    console.error('Database Connection Error:', error);
+  });
 
 // Server
 app.listen(port, () => {
