4f8184aafa8c01b634abcaa5513e561308d96a94
fredyyfajarr <fredyfajar@gmail.com>
2024-12-07 09:04:16 +0700

    update 3

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import ExpressMongoSanitize from 'express-mongo-sanitize';
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 import authRouter from './routes/authRouter.js';
 import productRouter from './routes/productRouter.js';
